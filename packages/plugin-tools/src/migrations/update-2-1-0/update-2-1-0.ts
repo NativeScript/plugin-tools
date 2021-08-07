6fe757eedf7710aa461e94baa3872f12150319b1
Nathan Walker <walkerrunpdx@gmail.com>
2021-08-07 10:18:16 -0700

    chore: use latest Nx 12.6 and bump TS to 4.3.5

--- packages/plugin-tools/src/migrations/update-2-1-0/update-2-1-0.ts
+++ packages/plugin-tools/src/migrations/update-2-1-0/update-2-1-0.ts
@@ -1,13 +1,12 @@
-import { Tree } from '@nrwl/devkit';
+import { Tree, updateJson } from '@nrwl/devkit';
 import { join } from 'path';
 import * as fs from 'fs';
 
 export default async function (tree: Tree) {
   const cwd = process.cwd();
+  // update build-finish to support non-scoped npm names
   const buildFinishPath = join(cwd, 'tools', 'scripts', 'build-finish.ts');
-  // console.log('webpackConfigPath:', webpackConfigPath);
   const buildFinishUpdatePath = join(cwd, `node_modules/@nativescript/plugin-tools/src/migrations/update-2-1-0/_files/build-finish`);
-  // console.log('webpackConfigPath:', webpackConfigPath);
   const buildFinish = fs.readFileSync(buildFinishPath, {
     encoding: 'utf-8',
   });
@@ -15,6 +14,24 @@ export default async function (tree: Tree) {
     encoding: 'utf-8',
   });
   if (buildFinish && buildFinishPath) {
-    tree.write(buildFinishPath, buildFinishUpdate);
+    tree.write('tools/scripts/build-finish.ts', buildFinishUpdate);
   }
+
+  // update jest.config to latest config
+  const jestConfigPath = join(cwd, 'jest.config.js');
+  const jestConfigUpdatePath = join(cwd, `node_modules/@nativescript/plugin-tools/src/migrations/update-2-1-0/_files/jest.config`);
+  const jestConfig = fs.readFileSync(jestConfigPath, {
+    encoding: 'utf-8',
+  });
+  const jestConfigUpdate = fs.readFileSync(jestConfigUpdatePath, {
+    encoding: 'utf-8',
+  });
+  if (jestConfig && jestConfigUpdate) {
+    tree.write('jest.config.js', jestConfigUpdate);
+  }
+
+  updateJson(tree, 'package.json', (json) => {
+    json.devDependencies['typescript'] = '4.3.5';
+    return json;
+  });
 }
